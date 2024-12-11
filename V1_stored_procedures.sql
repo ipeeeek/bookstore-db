402b633eba172c95c69e19aacf188a1603638ad4
Ipek Yalcin <110557214+ipeeeek@users.noreply.github.com>
2024-12-11 18:14:17 +0300

    Update V1_stored_procedures.sql
    -feat: Added USP for order processing (usp_process_order)
    
    -style: Fixed indentation of column names of INSERT INTO and VALUES statements.

--- V1_stored_procedures.sql
+++ V1_stored_procedures.sql
@@ -1,13 +1,30 @@
 USE bookstore_dev;
 GO
 
-/*CREATE PROCEDURE usp_process_order
+CREATE PROCEDURE usp_process_order
 	@customer_id INT,
-	@order_date
-	@
-	@
-	@,
-*/
+	@shipping_address_id INT,
+	@order_status_id INT,
+	@payment_id INT,
+	@total_amount DECIMAL(18,2)
+AS
+BEGIN
+	INSERT INTO customer_order (
+		customer_id,
+		shipping_address_id,
+		order_status_id,
+		payment_id,
+		total_amount
+	)
+	VALUES (
+		@customer_id,
+		@shipping_address_id,
+		@order_status_id,
+		@payment_id,
+		@total_amount
+	);
+END;
+GO
 
 CREATE PROCEDURE usp_add_shipping_address
 	@street_id INT,
@@ -19,8 +36,22 @@ CREATE PROCEDURE usp_add_shipping_address
 	@shipping_address_id INT OUTPUT
 AS
 BEGIN
-	INSERT INTO shipping_address (street_id, neighborhood_id, district_id, province_id, country_id, postal_code_id)
-	VALUES (@street_id, @neighborhood_id, @district_id, @province_id, @country_id, @postal_code_id)
+	INSERT INTO shipping_address (
+	street_id,
+	neighborhood_id,
+	district_id,
+	province_id,
+	country_id,
+	postal_code_id
+	)
+	VALUES (
+	@street_id,
+	@neighborhood_id,
+	@district_id,
+	@province_id,
+	@country_id,
+	@postal_code_id
+	)
 
 	SET @shipping_address_id = SCOPE_IDENTITY();
 END;
